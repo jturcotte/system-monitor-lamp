9276f3b8ec5065ee098f2d02c1d2d113bb48431c
Jocelyn Turcotte <turcotte.j@gmail.com>
2018-10-10 22:16:18 +0200

    Add disk stats

--- service/src/main.rs
+++ service/src/main.rs
@@ -23,7 +23,7 @@ impl CpuInfo {
         }
     }
 
-    pub fn fetch(&mut self) -> Vec<f32> {
+    pub fn fetch_stats(&mut self) -> Vec<f32> {
         let vals = {
             let f = BufReader::new(File::open("/proc/stat").unwrap());
             let it = f.lines()
@@ -67,7 +67,7 @@ impl NetInfo {
         }
     }
 
-    pub fn fetch(&mut self) -> (u32, u32) {
+    pub fn fetch_stats(&mut self) -> (u32, u32) {
         let f = BufReader::new(File::open("/proc/net/dev").unwrap());
         let vals = f.lines()
             .filter_map(|r| {
@@ -87,13 +87,55 @@ impl NetInfo {
 
         if self.last_vals.is_empty() {
             self.last_vals = vals.clone();
-        } 
+        }
 
         let bytes = self.last_vals
             .iter()
             .zip(vals.clone())
             .map(|(&(last_recv, last_sent), (recv, sent))| (recv - last_recv, sent - last_sent))
             .fold((0, 0), |(acc_recv, acc_sent), (recv, sent)| (acc_recv + recv, acc_sent + sent));
+        self.last_vals = vals;
+
+        bytes
+    }
+}
+
+pub struct DiskInfo {
+    last_vals: Vec<(u64, u64)>,
+}
+
+impl DiskInfo {
+    pub fn new() -> DiskInfo {
+        DiskInfo {
+            last_vals: Vec::new()
+        }
+    }
+
+    pub fn fetch_stats(&mut self) -> (u64, u64) {
+        let f = BufReader::new(File::open("/proc/diskstats").unwrap());
+        let vals = f.lines()
+            .filter_map(|r| {
+                let line = r.unwrap();
+                let split = line.split_whitespace().collect::<Vec<_>>();
+                if split[0] == "8" && split[1] == "0" {
+                    let read_bytes = split[5].parse::<u64>().unwrap() * 512;
+                    let written_bytes = split[9].parse::<u64>().unwrap() * 512;
+                    Some((read_bytes, written_bytes))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        if self.last_vals.is_empty() {
+            self.last_vals = vals.clone();
+        }
+
+        let bytes = self.last_vals
+            .iter()
+            .zip(vals.clone())
+            .map(|(&(last_read, last_written), (read, written))| (read - last_read, written - last_written))
+            .fold((0, 0), |(acc_read, acc_written), (read, written)| (acc_read + read, acc_written + written));
         self.last_vals = vals;
 
         bytes
@@ -137,20 +179,23 @@ fn main() {
     let duration = time::Duration::from_millis(2000);
     let mut cpu_info = CpuInfo::new();
     let mut net_info = NetInfo::new();
+    let mut disk_info = DiskInfo::new();
     loop {
-        let percents = cpu_info.fetch();
+        let cpu_stats = cpu_info.fetch_stats();
 
         println!("Starting");
-        for p in &percents {
+        for p in &cpu_stats {
             println!("\tCPU {}", p);
         }
 
-        let bytes = net_info.fetch();
-        println!("NET {} {}", bytes.0, bytes.1);
+        let disk_stats = disk_info.fetch_stats();
+        println!("DISK {} {}", disk_stats.0, disk_stats.1);
+        let net_stats = net_info.fetch_stats();
+        println!("NET {} {}", net_stats.0, net_stats.1);
         let num_leds = 12;
         let led_per_cpu = num_leds / cpu_info.last_vals.len();
-        let led_per_net_param = num_leds / 2;
-        let cpu_leds_iter = percents
+        let led_per_access_param = num_leds / 2;
+        let cpu_leds_iter = cpu_stats
             .iter()
             .flat_map(|p| {
                 let mut dst = Vec::new();
@@ -159,20 +204,26 @@ fn main() {
                 }
                 dst
             });
+
+        let mut disk_read_leds = Vec::new();
+        let mut disk_written_leds = Vec::new();
         let mut net_recv_leds = Vec::new();
         let mut net_sent_leds = Vec::new();
-        for _ in 0..led_per_net_param {
-            net_recv_leds.push((255. * bytes.0 as f32 / 1000000.) as u8);
-            net_sent_leds.push((255. * bytes.1 as f32 / 100000.) as u8);
+        for _ in 0..led_per_access_param {
+            disk_read_leds.push((255. * disk_stats.0 as f64 / 200000000.) as u8);
+            disk_written_leds.push((255. * disk_stats.1 as f64 / 200000000.) as u8);
+            net_recv_leds.push((255. * net_stats.0 as f32 / 1000000.) as u8);
+            net_sent_leds.push((255. * net_stats.1 as f32 / 100000.) as u8);
         }
+
         let leds_iter = cpu_leds_iter
+            .zip(disk_read_leds.iter().chain(disk_written_leds.iter()))
             .zip(net_recv_leds.iter().chain(net_sent_leds.iter()))
-            .zip(std::iter::repeat(0u8).take(num_leds))
             .flat_map(|((r, g), b)| {
                 let mut dst = Vec::new();
                 dst.push(r);
                 dst.push(*g);
-                dst.push(b);
+                dst.push(*b);
                 dst
             });
 
